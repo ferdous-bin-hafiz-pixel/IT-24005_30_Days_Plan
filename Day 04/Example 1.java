43515f6d7cb9bd4a9d3e7b835978fafa392b3f39
ferdous-bin-hafiz-pixel <ferdousbinhafizshaccha@gmail.com>
2026-04-27 01:01:09 +0600

    Add files via upload

---
+++ Day 04/Example 1.java
@@ -0,0 +1,19 @@
+class Animal {
+    void eat() {
+        System.out.println("Eating...");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Barking...");
+    }
+}
+
+public class TestInheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();   // inherited method
+        d.bark();  // own method
+    }
+}
